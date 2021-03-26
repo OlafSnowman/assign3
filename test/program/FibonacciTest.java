6257bba90699bc8e874c46ecd327c11d09f133e4
Christian Montemayor <christianamontemayor@gmail.com>
2021-03-26 17:34:31 -0500

    Initial commit

---
+++ test/program/FibonacciTest.java
@@ -0,0 +1,12 @@
+package program;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FibonacciTest {
+  @Test
+  public void Canary() {
+    assertTrue(true);
+  }
+}
